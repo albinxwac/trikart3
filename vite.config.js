f3076470cff0c33d7f290388ebb3b402ede79115
albinxwac <albin.s@webandcrafts.in>
2025-03-13 10:13:52 +0530

    changed vite config to fix refresh issue

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  
   plugins: [react()],
-})
+  server: {port: 5174},
+  alias: {
+    '@': '/src', // Ensure this points to your src directory
+  },
+});
